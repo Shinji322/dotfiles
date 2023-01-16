4a46789f412fe76525a7edd600ee0983a00e36cf
Ronan Maharaj <admin@ronanm.me>
2023-01-16 09:43:14 -0500

    Update nvim config

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -8,4 +8,11 @@ return {
   ["gpanders/editorconfig.nvim"] = {
 
   },
+
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
 }
