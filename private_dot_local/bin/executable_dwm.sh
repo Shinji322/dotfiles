39230f144139574bc4d580bf1b9ad3f0c3092c5c
Ronan Maharaj <admin@ronanm.me>
2023-02-06 17:24:52 -0500

    feb06

--- private_dot_local/bin/executable_dwm.sh
+++ private_dot_local/bin/executable_dwm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 (sleep 1; dwmblocks) &
+(command -v autorandr && autorandr -c && arpid=$!) & # automatically set resolution and stuff
+([ -n "$arpid" ] && wait "$arpid"; setbg) &
 
 # Allows you to restart dwm without killing Xorg 
 while true; do
