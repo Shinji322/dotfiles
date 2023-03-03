1637e70a2923dc68ae828d42ca15eaa92e38c753
Ronan Maharaj <admin@ronanm.me>
2023-03-02 21:57:58 -0500

    02/03

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -7,3 +7,13 @@ require("custom.neovim").config()
 -- Autocompiling
 vim.api.nvim_set_keymap("n", "<S-F5>", ":!compiler %", {silent=true})
 vim.api.nvim_set_keymap("n", "<F5>", ":!compiler %<CR>", {silent=true})
+
+-- Autocommands
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufWrite", {
+  pattern = "xresources",
+  callback = function()
+    os.execute("xrdb -merge ~/.config/x11/xresources")
+  end,
+})
