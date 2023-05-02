7b4097a00626e587657bec1221097687710378a6
Ronan Maharaj <admin@ronanm.me>
2023-05-02 14:29:15 -0400

    minimal steam

---
+++ private_dot_local/bin/games/executable_steam.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# shellcheck disable=SC2068
+HOME="$HOME/.local/share/steam"
+mkdir -pv "$HOME/.local/share"
+steam -dev -console -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -cef-force-32bit -no-cef-sandbox -vrdisable -cef-disable-breakpad $@
